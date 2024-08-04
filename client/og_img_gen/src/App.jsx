693d3c750b85aa2dcde24e33c101567eee078226
Manveer <manveer7saggu@gmail.com>
2024-08-04 23:44:55 +0530

    fix

--- client/og_img_gen/src/App.jsx
+++ client/og_img_gen/src/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import PostPage from './Components/PostPage'
+import PostPage from './components/PostPage'
 
 export default function App() {
-      const post = {
-    title: 'Sample Post Title',
-    content: 'This is a sample post content.',
-    imageUrl: 'https://images.pexels.com/photos/1004584/pexels-photo-1004584.jpeg?auto=compress&cs=tinysrgb&w=600',
-  };
 
-  return <PostPage post={post} />;
+  return <PostPage />;
 }
